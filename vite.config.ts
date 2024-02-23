3c37b2a826eac05a019b48dcf054520deb99d056
ouyang <xiouyang2023@163.com>
2024-02-23 17:41:09 +0800

    feat: 9-4-1、安装antd-vue

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,22 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    // antd-vue 按需引入
+    Components({
+      resolvers: [
+        AntDesignVueResolver({
+          importStyle: false // css in js
+        })
+      ]
+    })
   ],
   resolve: {
     alias: {
